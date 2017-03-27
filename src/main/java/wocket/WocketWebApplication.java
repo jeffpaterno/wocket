8ca1514147e9e34fdbb36d66b10ac70f31faef82
Jeff Paterno <jeffpaterno@Jeffs-MacBook-Pro.local>
2017-03-26 20:22:57 -0400

    Setup Gradle web application

---
+++ src/main/java/wocket/WocketWebApplication.java
@@ -0,0 +1,12 @@
+package wocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WocketWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WocketWebApplication.class, args);
+	}
+}
